027fbfaac6247a0cee54c6a3597ed460bb6822ac
CodeSandbox <support@codesandbox.io>
2025-05-18 15:38:49 +0000

    remove all

--- resources/js/Components/ApplicationLogo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="relative flex h-9 w-9 items-center justify-center overflow-hidden rounded-xl bg-gradient-to-br from-blue-600 to-blue-700 shadow-lg shadow-blue-600/20 ring-1 ring-white/10">
-        <div class="absolute inset-0 bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        <span class="relative text-xl font-bold text-white">D</span>
-    </div>
-</template>
